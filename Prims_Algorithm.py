176256e4e2c58bb3792c6acfbddeaed3952e235d
samsundar <samradnus2001@gmail.com>
2022-06-13 08:55:52 +0530

    Prim's Algorithm

---
+++ Prims_Algorithm.py
@@ -0,0 +1,23 @@
+import heapq
+
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        heap = [(0, 0)]  # min heap
+        mst_cost = 0
+        edges_used = 0
+        in_mst = [False] * n
+        while edges_used < n:
+            weight, curr_node = heapq.heappop(heap)
+            if in_mst(curr_node):
+                continue
+            mst_cost[curr_node]+=weight
+            in_mst[curr_node] = True
+            edges_used += 1
+            for next_node in range(n):
+                if not in_mst(next_node):
+                    next_weight = abs(points[curr_node][0] - points[next_node][0]) + \
+                                  abs(points[curr_node][1] - points[next_node][1])
+                    heapq.heappush(heap,(next_weight,next_node))
+        return mst_cost
